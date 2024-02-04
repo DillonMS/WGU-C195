52abeac4cd43d6aaa00fc4e68f19c7ac21419914
dillon <dilloncan@hotmail.com>
2024-02-03 20:14:42 -0500

    javadocs

--- src/main/java/smith/c195v2/helper/CustomerQuery.java
+++ src/main/java/smith/c195v2/helper/CustomerQuery.java
@@ -3,8 +3,20 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * all sql queries based on the customer table
+ */
 public abstract class CustomerQuery {
 
+    /**
+     * Inserts customer into database.
+     * @param name
+     * @param address
+     * @param postalCode
+     * @param phoneNumber
+     * @param dID
+     * @throws SQLException
+     */
     public static void insertCustomer(String name, String address, String postalCode, String phoneNumber, int dID) throws SQLException {
         String sql = "INSERT INTO customers (Customer_Name, Address, Postal_Code, Phone, Division_ID) VALUES(?, ?, ?, ?, ?)";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
@@ -16,12 +28,27 @@ public abstract class CustomerQuery {
         ps.executeUpdate();
     }
 
+    /**
+     * removes customer from database
+     * @param customerID customer to delete.
+     * @throws SQLException
+     */
     public static void removeCustomer(int customerID) throws SQLException {
         String sql = "DELETE FROM customers WHERE Customer_ID = " + customerID +";";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
         ps.executeUpdate();
     }
 
+    /**
+     * Updates customer based on the ID given.
+     * @param customerID
+     * @param name
+     * @param address
+     * @param postalCode
+     * @param phoneNumber
+     * @param dID
+     * @throws SQLException
+     */
     public static void updateCustomer(int customerID, String name, String address, String postalCode, String phoneNumber, int dID) throws SQLException{
         String sql = "UPDATE customers SET Customer_Name = ?, Address = ?, Postal_Code = ?, Phone = ?, Division_ID = ? WHERE Customer_ID = ?";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
@@ -35,6 +62,12 @@ public abstract class CustomerQuery {
 
     }
 
+    /**
+     * get the first level division based on the division ID provided
+     * @param divisionID
+     * @return String of the first level division
+     * @throws SQLException
+     */
     public static String getFirstLevelDivision(int divisionID) throws SQLException {
         String sql = "SELECT Division FROM first_level_divisions WHERE Division_ID = ?";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
@@ -47,6 +80,12 @@ public abstract class CustomerQuery {
             return "";
     }
 
+    /**
+     * returns ID based on the first level division provided
+     * @param division
+     * @return ID
+     * @throws SQLException
+     */
     public static int getDivisionID(String division) throws SQLException {
         String sql = "SELECT Division_ID FROM first_level_divisions WHERE Division = ?";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
@@ -60,7 +99,12 @@ public abstract class CustomerQuery {
     }
 
 
-
+    /**
+     * get the country based on the division ID
+     * @param divisionID used to get the country
+     * @return String of country
+     * @throws SQLException
+     */
     public static String getCountry(int divisionID) throws SQLException{
         String sql = "SELECT Country_ID FROM first_level_divisions WHERE Division_ID = ?";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
@@ -83,14 +127,19 @@ public abstract class CustomerQuery {
 
     }
 
+    /**
+     * Checks whether there is an appointment for a given customer ID.  Cannot delete customer if
+     * customer has appointment.
+     * @param customerID
+     * @return return true or false
+     * @throws SQLException
+     */
     public static boolean customerAppointments(int customerID) throws SQLException {
         String sql = "SELECT * FROM appointments WHERE Customer_ID = ?";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
         ps.setInt(1,customerID);
         ResultSet rs = ps.executeQuery();
         return rs.next();
-
-
     }
 }
 
