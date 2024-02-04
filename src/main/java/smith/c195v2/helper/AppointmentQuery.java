c3ff069698c30262fefb743300a70eb1fe9a9ef8
dillon <dilloncan@hotmail.com>
2024-02-03 20:10:05 -0500

    javadocs

--- src/main/java/smith/c195v2/helper/AppointmentQuery.java
+++ src/main/java/smith/c195v2/helper/AppointmentQuery.java
@@ -15,8 +15,24 @@ import java.time.temporal.TemporalField;
 import java.time.temporal.WeekFields;
 import java.util.Locale;
 
+/**
+ * Contains all methods containing sql queries involving the appointments table
+ */
 public abstract class AppointmentQuery {
 
+    /**
+     * Inserts appointment into the database.
+     * @param title
+     * @param description
+     * @param location
+     * @param type
+     * @param start
+     * @param end
+     * @param customerID
+     * @param userID
+     * @param contactID
+     * @throws SQLException
+     */
     public static void insertAppointment(String title, String description, String location, String type, String start, String end, int customerID, int userID, int contactID) throws SQLException {
         String sql = "INSERT INTO appointments (Title, Description, Location, Type, Start, End, Customer_ID, User_ID, Contact_ID) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
@@ -32,6 +48,20 @@ public abstract class AppointmentQuery {
         ps.executeUpdate();
     }
 
+    /**
+     * Updates existing appointment in the database. Does not change ID.
+     * @param title
+     * @param description
+     * @param location
+     * @param type
+     * @param start
+     * @param end
+     * @param customerID
+     * @param userID
+     * @param contactID
+     * @param appointmentID
+     * @throws SQLException
+     */
     public static void modifyAppointment(String title, String description, String location, String type, String start, String end, int customerID, int userID, int contactID, int appointmentID) throws SQLException {
         String sql = "Update appointments SET Title = ?, Description = ?, Location = ?, type = ?, Start = ?, End = ?, Customer_ID = ?, User_ID = ?, Contact_ID =? WHERE Appointment_ID = ?";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
@@ -48,12 +78,23 @@ public abstract class AppointmentQuery {
         ps.executeUpdate();
     }
 
+    /**
+     * Removes appointment from the database.
+     * @param appointmentID selects which appointment to remove.
+     * @throws SQLException
+     */
     public static void removeAppointment(int appointmentID) throws SQLException {
         String sql = "DELETE FROM appointments WHERE Appointment_ID = " + appointmentID +";";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
         ps.executeUpdate();
     }
 
+    /**
+     * Gets the contact name based on its ID.
+     * @param contactID used to get String
+     * @return returns Name
+     * @throws SQLException
+     */
     public static String getContact(int contactID) throws SQLException {
         String sql = "SELECT Contact_Name FROM contacts WHERE Contact_ID = ?";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
@@ -65,6 +106,11 @@ public abstract class AppointmentQuery {
             return "";
     }
 
+    /**
+     * returns All appointments for the main screen all view.
+     * @return Observable list of all apointments.
+     * @throws SQLException
+     */
     public static ObservableList<Appointment> getAllAppointments() throws SQLException {
 
         ObservableList<Appointment> appointmentList = FXCollections.observableArrayList();
@@ -106,6 +152,12 @@ public abstract class AppointmentQuery {
         return appointmentList;
     }
 
+    /**
+     * Returns only the appointments for the month specified.
+     * @param date Used for year and month.
+     * @return All appointments for the month.
+     * @throws SQLException
+     */
     public static ObservableList<Appointment> getMonthAppointments(LocalDate date) throws SQLException {
         ObservableList<Appointment> monthList = FXCollections.observableArrayList();
 
@@ -150,6 +202,12 @@ public abstract class AppointmentQuery {
         return monthList;
     }
 
+    /**
+     * Returns all appointments based on the week given
+     * @param date Used to extract which week in the year it is.
+     * @return All appointments within the week.
+     * @throws SQLException
+     */
     public static ObservableList<Appointment> getWeekAppointments(LocalDate date) throws SQLException {
         ObservableList<Appointment> weekList = FXCollections.observableArrayList();
 
@@ -198,6 +256,12 @@ public abstract class AppointmentQuery {
         return weekList;
     }
 
+    /**
+     * Returns the closest appointment to the current date and time. This will be used to determine
+     * if an appointment is starting in 15 minutes or less.
+     * @return Single appointment
+     * @throws SQLException
+     */
     public static Appointment returnClosestAppointment() throws SQLException {
         LocalDateTime lDT = LocalDateTime.now();
         LocalDateTime lDTUTC = TimeConversions.convertToUTC(lDT);
@@ -237,7 +301,12 @@ public abstract class AppointmentQuery {
         return null;
     }
 
-
+    /**
+     * Get the user ID based on the name provided
+     * @param userName name provided.
+     * @return user ID.
+     * @throws SQLException
+     */
     public static int getUserID(String userName) throws SQLException {
         String sql = "SELECT User_ID FROM users WHERE User_Name = ?";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
@@ -249,6 +318,12 @@ public abstract class AppointmentQuery {
             return -1;
     }
 
+    /**
+     * Get the username based on the ID provided
+     * @param userID used to get name
+     * @return String username
+     * @throws SQLException
+     */
     public static String getUserName(int userID) throws SQLException {
         String sql = "SELECT User_Name FROM users WHERE User_ID = ?";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
@@ -260,7 +335,12 @@ public abstract class AppointmentQuery {
             return "";
     }
 
-
+    /**
+     * Get the Contact ID based on the name
+     * @param contactName Name provided.
+     * @return ID.
+     * @throws SQLException
+     */
     public static int getContactID(String contactName) throws SQLException {
         String sql = "SELECT Contact_ID FROM contacts WHERE contact_Name = ?";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
@@ -272,6 +352,13 @@ public abstract class AppointmentQuery {
             return -1;
     }
 
+    /**
+     * Checks if a program already exists during the current time.
+     * @param start start of appointment in question
+     * @param end end of appointment in question
+     * @return true or false if there is an overlapping appointment
+     * @throws SQLException
+     */
     public static Boolean overlappingAppointments(LocalDateTime start, LocalDateTime end) throws SQLException {
         String sql = "SELECT * FROM appointments WHERE Start BETWEEN ? AND ? OR End Between ? AND ?";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
@@ -283,6 +370,14 @@ public abstract class AppointmentQuery {
         return rs.next();
     }
 
+    /**
+     * Checks if a program already exists during the current time.
+     * @param start start of appointment in question
+     * @param end end of appointment in question
+     * @param id used to exclude the current appointment when modifying
+     * @return true or false if there is an overlapping appointment
+     * @throws SQLException
+     */
     public static Boolean overlappingAppointments(LocalDateTime start, LocalDateTime end, int id) throws SQLException {
         String sql = "SELECT * FROM appointments WHERE Start BETWEEN ? AND ? AND Appointment_ID != ? OR End Between ? AND ? AND Appointment_ID != ?";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
